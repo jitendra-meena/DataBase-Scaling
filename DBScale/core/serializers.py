2f97c828d97ad1867f85e46988b1ecafc74d9e3a
Jitendra Meena <jitendrameena9575@gmail.com>
2022-03-28 22:38:19 +0530

    changes

--- DBScale/core/serializers.py
+++ DBScale/core/serializers.py
@@ -6,6 +6,7 @@ class AddCustomerSerializer(serializers.ModelSerializer):
     model = Customer
     fields ='__all__'
 
+
 class CustomerSerializer(serializers.ModelSerializer):
   class Meta:
     model = CustomerSupport
