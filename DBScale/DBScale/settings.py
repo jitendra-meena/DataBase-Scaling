d79989b06dba39e53d6a203b683327670a8434dd
Jitendra Meena <jitendrameena9575@gmail.com>
2022-04-05 13:05:21 +0530

    Multiple Databases

--- DBScale/DBScale/settings.py
+++ DBScale/DBScale/settings.py
@@ -85,13 +85,17 @@ WSGI_APPLICATION = 'DBScale.wsgi.application'
 # }
 # Multiples DataBases
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'DBscaling',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DBscaling',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     },
     'user': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
