01b84482c8831d155a29f91ad766057006e06e2f
Jitendra Meena <jitendrameena9575@gmail.com>
2022-04-05 13:27:07 +0530

    Customer API Changes

--- DBScale/core/views.py
+++ DBScale/core/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .serializers import AddCustomerSerializer,CustomerSerializer
+from .serializers import AddCustomerSerializer, CustomerSerializer
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView
-from .models import Customer,CustomerSupport
+from .models import Customer, CustomerSupport
 import logging
 from drf_yasg import openapi
 from rest_framework_swagger.views import get_swagger_view
@@ -20,41 +20,42 @@ schema_view = get_schema_view(
         terms_of_service="https://www.jaseci.org",
         contact=openapi.Contact(email="jason@jaseci.org"),
         license=openapi.License(name="Awesome IP"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 # TEST DataBase Scaling ...
-class AddCustomer(APIView): 
-  def get(self,request):
-    customer = CustomerSupport.objects.all()
-    serializer = AddCustomerSerializer(customer,many=True)
-    logging.info("User Data Created")
-    return Response(serializer.data) 
-  
-  def post(self, request):
-    serializer = AddCustomerSerializer(data=request.data)
-    print(serializer)
-    if serializer.is_valid():
-      serializer.save() 
-      return Response(serializer.data, status=status.HTTP_200_OK)
-    logging.warning("Data Not Valid Error")
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AddCustomer(APIView):
+    def get(self, request):
+        customer = CustomerSupport.objects.all()
+        serializer = AddCustomerSerializer(customer, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AddCustomerSerializer(data=request.data)
+        print(serializer)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Add Multiples users in same time using this api
+
+
 class Customer(ListCreateAPIView):
-  queryset = CustomerSupport.objects.all()
-  serializer_class = CustomerSerializer
-  def post(self, request, *args, **kwargs):
-    name = request.data.get('name')
-    address = request.data.get('address')
-    city = request.data.get('city')
-    customer = CustomerSupport.objects.using('replica').create(name=name, address=address, city=city)
-    print("DataBase Scaling")
-    customer.save()
-    customers = CustomerSupport.objects.all()  
-    serializer = CustomerSerializer(customers,many=True)
-    return Response({'msg':'Custom List','data':serializer.data})   
+    queryset = CustomerSupport.objects.all()
+    serializer_class = CustomerSerializer
 
-
+    def post(self, request, *args, **kwargs):
+        name = request.data.get('name')
+        address = request.data.get('address')
+        city = request.data.get('city')
+        customer = CustomerSupport.objects.using('replica').create(
+            name=name, address=address, city=city)
+        customer.save()
+        customers = CustomerSupport.objects.all()
+        serializer = CustomerSerializer(customers, many=True)
+        return Response({'msg': 'Custom List', 'data': serializer.data})
