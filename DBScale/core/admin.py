ad399ea937cc53fea0379840c7553eed70133bfc
Jitendra Meena <jitendrameena9575@gmail.com>
2022-04-06 15:08:00 +0530

    Multiple DataBases

--- DBScale/core/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from core.models import Customer, CustomerSupport, CustomerInformation
-
-admin.site.register(Customer)
-admin.site.register(CustomerSupport)
-admin.site.register(CustomerInformation)
--- DBScale/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from . views import *
-urlpatterns = [
-  path('add_customer/',AddCustomer.as_view(),name="add_customer"),
-  path('customer/',Customer.as_view(),name="customer")
-
-  
-]
